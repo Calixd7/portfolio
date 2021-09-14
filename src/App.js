34dd4d47ee2f983a8ae3527149822524c165f47a
David Calix <calixd7@gmail.com>
2021-09-13 21:39:46 -0400

    setup react files, styled components setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+import styled from "styled-components";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <AppStyled>
+     <h1>Styled Components</h1>
+   </AppStyled>
   );
 }
-
+const AppStyled = styled.div`
+ background-color: red;`
 export default App;
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle `
+*{
+    padding: 0;
+    margin:0;
+    box-sizing: border-box;
+    font-size: 1.2 rem;
+}
+    body{
+        background-color: #333;
+    }
+`;
+
+export default GlobalStyle;
